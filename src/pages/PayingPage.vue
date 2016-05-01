9659a9714ade852a47b4be3dfdcdb998a2ef58a4
Adam Drescher <dressi86@gmail.com>
2016-05-01 21:54:16 +0200

    Misc

--- src/pages/PayingPage.vue
+++ src/pages/PayingPage.vue
@@ -1,20 +1,6 @@
 <template>
     <div class="purchase_process">
         <header-bar></header-bar>
-
-        <div class="cart">
-            <h2>Your Cart</h2>
-            <p v-show="!tickets.length"><i>Please add some ticket to cart.</i></p>
-            <ul>
-                <li v-for="p in tickets">
-                    {{ p.title }} - {{ p.price | currency }} x {{ p.quantity }}
-                </li>
-            </ul>
-            <p>Total: {{ total | currency }}</p>
-            <p><button :disabled="!tickets.length" @click="checkout(tickets)">Checkout</button></p>
-            <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
-        </div>
-
         <div class="content">
             <div class="paying_page process_page is_last_process_page">
                 <var>End Price</var>
